59dc54544252a59637829bdf6c8ad78e6f0637df
Pedro Rodrigues <pedromiguelrodrigues2000@gmail.com>
2022-12-16 19:41:30 +0000

    Commit This before pulling

--- src/ThirdChallenge/app/src/main/java/com/example/thirdchallenge/activities/MainActivity.java
+++ src/ThirdChallenge/app/src/main/java/com/example/thirdchallenge/activities/MainActivity.java
@@ -30,15 +30,6 @@ import kotlinx.coroutines.channels.Channel;
 
 public class MainActivity extends AppCompatActivity implements FragmentChanger {
 
-    public static final String LED_TOPIC = "/challenge/led";
-    public static final String TEMPERATURE_TOPIC = "/challenge/temperature";
-    public static final String HUMIDITY_TOPIC = "/challenge/humidity";
-
-    private MQTT mqttService;
-    private DashboardViewModel viewModel;
-
-    private static final String CHANNEL_ID = "App";
-
     public MainActivity() {
         super(R.layout.activity_main);
     }
@@ -48,28 +39,12 @@ public class MainActivity extends AppCompatActivity implements FragmentChanger {
         super.onCreate(savedInstanceState);
 
         // Init Shared View Model With Data
-        this.viewModel = new ViewModelProvider(this).get(DashboardViewModel.class);
-
-        // Start Mqtt Service
-        this.mqttService = setupMqttService();
+        new ViewModelProvider(this).get(DashboardViewModel.class);
 
         // Instantiate Display Dashboard Fragment
         replaceFragment(new DashboardFragment());
-
-        // Init Notification Channel
-        createNotificationsChannel();
     }
 
-    private void createNotificationsChannel() {
-        int importance = NotificationManager.IMPORTANCE_DEFAULT;
-        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "ThirdChallenge", importance);
-        channel.setDescription("Third Challenge Notifications");
-        // Register the channel with the system; you can't change the importance
-        // or other notification behaviors after this
-        NotificationManager notificationManager = getSystemService(NotificationManager.class);
-        notificationManager.createNotificationChannel(channel);
-
-    }
 
     @Override
     public void replaceFragment(Fragment fragment) {
@@ -78,60 +53,5 @@ public class MainActivity extends AppCompatActivity implements FragmentChanger {
         fragmentTransaction.replace(R.id.fragment_container, fragment, fragment.toString());
         fragmentTransaction.addToBackStack(fragment.toString());
         fragmentTransaction.commit();
-    }
-
-    private MQTT setupMqttService() {
-        MQTT mqttService = new MQTT(getApplicationContext(), "dashboard-app", "lol");
-        mqttService.setCallback(new MqttCallbackExtended() {
-            boolean notify = true;
-            @Override
-            public void connectComplete(boolean reconnect, String serverURI) {
-                System.err.println("CONNECTED: " + serverURI);
-                // Default Subscribe Topic
-                mqttService.subscribeToTopic(TEMPERATURE_TOPIC);
-                mqttService.subscribeToTopic(HUMIDITY_TOPIC);
-            }
-
-            @Override
-            public void connectionLost(Throwable cause) {
-                System.err.println("CONNECTION LOST!");
-            }
-
-            @Override
-            public void messageArrived(String topic, MqttMessage message) {
-                Measure<Double> measure = new Measure(Double.parseDouble(message.toString()));
-                if (topic.equals(TEMPERATURE_TOPIC)) {
-                    viewModel.addTemperatureMeasure(measure);
-                    if (notify && (measure.getMeasure() < viewModel.getMinTemperature()
-                            || measure.getMeasure() > viewModel.getMaxTemperature())) {
-                        System.out.println("here");
-                        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID)
-                                .setSmallIcon(R.drawable.ic_launcher_background)
-                                .setContentTitle("Temperature Warning!")
-                                .setContentText("Measured temperature exceeded allowed range!")
-                                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-                        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getApplicationContext());
-                        notificationManager.notify(1, builder.build());
-                        notify = false;
-                    } else {
-                        notify = true;
-                    }
-
-                } else if (topic.equals(HUMIDITY_TOPIC)) {
-                    viewModel.addHumidityMeasure(measure);
-                }
-            }
-
-            @Override
-            public void deliveryComplete(IMqttDeliveryToken token) {
-                System.out.println("DELIVERED!");
-            }
-        });
-        mqttService.connect();
-        return mqttService;
-    }
-
-    public MQTT getMqttService() {
-        return this.mqttService;
     }
 }
--- src/ThirdChallenge/app/src/main/java/com/example/thirdchallenge/fragments/DashboardFragment.java
+++ src/ThirdChallenge/app/src/main/java/com/example/thirdchallenge/fragments/DashboardFragment.java
@@ -4,11 +4,18 @@ import static com.example.thirdchallenge.activities.MainActivity.HUMIDITY_TOPIC;
 import static com.example.thirdchallenge.activities.MainActivity.LED_TOPIC;
 import static com.example.thirdchallenge.activities.MainActivity.TEMPERATURE_TOPIC;
 
+import android.annotation.SuppressLint;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SwitchCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -22,10 +29,13 @@ import android.widget.TextView;
 import com.example.thirdchallenge.R;
 import com.example.thirdchallenge.activities.MainActivity;
 import com.example.thirdchallenge.models.DashboardViewModel;
+import com.example.thirdchallenge.models.Measure;
 import com.example.thirdchallenge.util.MQTT;
 import com.github.mikephil.charting.charts.LineChart;
 import com.google.android.material.slider.RangeSlider;
 
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 import java.nio.charset.StandardCharsets;
@@ -34,17 +44,25 @@ import java.util.List;
 
 public class DashboardFragment extends Fragment {
 
-    private Switch temperatureSwitch;
-    private Switch humiditySwitch;
+    public static final String LED_TOPIC = "/challenge/led";
+    public static final String TEMPERATURE_TOPIC = "/challenge/temperature";
+    public static final String HUMIDITY_TOPIC = "/challenge/humidity";
+
+    private static final String CHANNEL_ID = "App";
 
     private MQTT mqttService;
-    private LineChart chart;
-    private RangeSlider slider;
-
-    private boolean buttonState;
-    private ImageButton button;
-
     private DashboardViewModel viewModel;
+
+    private SwitchCompat temperatureSwitch;
+    private SwitchCompat humiditySwitch;
+
+    private RangeSlider temperatureSlider;
+    private RangeSlider humiditySlider;
+
+    private LineChart chart;
+
+    private ImageButton button;
+    private boolean buttonState;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -61,29 +79,30 @@ public class DashboardFragment extends Fragment {
         this.viewModel = new ViewModelProvider(requireActivity()).get(DashboardViewModel.class);
 
         // Fetch MqttService
-        this.mqttService = ((MainActivity) requireActivity()).getMqttService();
+        this.mqttService = setupMqttService();
 
-        this.temperatureSwitch = (Switch) view.findViewById(R.id.switch_temperature);
-        this.temperatureSwitch.setChecked(true);
-
-        this.humiditySwitch = (Switch) view.findViewById(R.id.switch_humidity);
-        this.humiditySwitch.setChecked(true);
+        // Init Notification Channel
+        createNotificationsChannel();
 
         // Led Button
-        this.button = (ImageButton) view.findViewById(R.id.image_button);
+        this.button = view.findViewById(R.id.image_button);
         this.buttonState = false;
 
-        // Threshold
-        this.slider = (RangeSlider) view.findViewById(R.id.slider);
+        // Temperature Switch
+        this.temperatureSwitch = view.findViewById(R.id.switch_temperature);
+        this.temperatureSwitch.setChecked(true);
+
+        // Humidity Switch
+        this.humiditySwitch = view.findViewById(R.id.switch_humidity);
+        this.humiditySwitch.setChecked(true);
+
+        // Thresholds
+        this.temperatureSlider = view.findViewById(R.id.slider_temperature);
+        this.humiditySlider = view.findViewById(R.id.slider_humidity);
 
         // Charts
-        this.chart = (LineChart) view.findViewById(R.id.chart);
-        this.chart.setDrawGridBackground(true);
-        this.chart.setTouchEnabled(true);
-        this.chart.setDragEnabled(true);
-        this.chart.setScaleEnabled(true);
-        this.chart.setPinchZoom(false);
-        this.chart.setBackgroundColor(Color.BLACK);
+        this.chart = view.findViewById(R.id.chart);
+        this.setupChart(this.chart);
 
         return view;
     }
@@ -125,13 +144,103 @@ public class DashboardFragment extends Fragment {
                 mqttService.unsubscribeTopic(HUMIDITY_TOPIC);
             }
         });
-        // Setup Threshold Slider
-        this.slider.addOnChangeListener((slider, value, fromUser) -> {
+
+        // Setup Threshold Temperature Slider
+        this.temperatureSlider.addOnChangeListener((slider, value, fromUser) -> {
             List<Float> values = slider.getValues();
             viewModel.setMaxTemperature(Collections.max(values));
             viewModel.setMinTemperature(Collections.min(values));
         });
+
+        // Setup Humidity Temperature Slider
+        this.humiditySlider.addOnChangeListener((slider, value, fromUser) -> {
+            List<Float> values = slider.getValues();
+            viewModel.setMaxHumidity(Collections.max(values));
+            viewModel.setMinHumidity(Collections.min(values));
+        });
     }
 
+    private MQTT setupMqttService() {
+        MQTT mqttService = new MQTT(requireContext(), "dashboard-app", "lol");
+        mqttService.setCallback(new MqttCallbackExtended() {
+            boolean notifyTemperature = true;
+            boolean notifyHumidity = true;
+            @Override
+            public void connectComplete(boolean reconnect, String serverURI) {
+                System.err.println("CONNECTED: " + serverURI);
+                // Default Subscribe Topic
+                mqttService.subscribeToTopic(TEMPERATURE_TOPIC);
+                mqttService.subscribeToTopic(HUMIDITY_TOPIC);
+            }
+
+            @Override
+            public void connectionLost(Throwable cause) {
+                System.err.println("CONNECTION LOST!");
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) {
+                Measure<Double> measure = new Measure(Double.parseDouble(message.toString()));
+                if (topic.equals(TEMPERATURE_TOPIC)) {
+                    viewModel.addTemperatureMeasure(measure);
+                    if (notifyTemperature && (measure.getMeasure() < viewModel.getMinTemperature()
+                            || measure.getMeasure() > viewModel.getMaxTemperature())) {
+                        System.out.println("here");
+                        NotificationCompat.Builder builder = new NotificationCompat.Builder(requireContext(), CHANNEL_ID)
+                                .setSmallIcon(R.drawable.ic_launcher_background)
+                                .setContentTitle("Temperature Warning!")
+                                .setContentText("Measured temperature exceeded allowed range!")
+                                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(requireContext());
+                        notificationManager.notify(1, builder.build());
+                        notifyTemperature = false;
+                    } else {
+                        notifyTemperature = true;
+                    }
+
+                } else if (topic.equals(HUMIDITY_TOPIC)) {
+                    viewModel.addHumidityMeasure(measure);
+                    if (notifyTemperature && (measure.getMeasure() < viewModel.getMaxHumidity()
+                            || measure.getMeasure() > viewModel.getMinHumidity())) {
+                        System.out.println("here");
+                        NotificationCompat.Builder builder = new NotificationCompat.Builder(requireContext(), CHANNEL_ID)
+                                .setSmallIcon(R.drawable.ic_launcher_background)
+                                .setContentTitle("Temperature Warning!")
+                                .setContentText("Measured temperature exceeded allowed range!")
+                                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(requireContext());
+                        notificationManager.notify(2, builder.build());
+                        notifyTemperature = false;
+                    } else {
+                        notifyTemperature = true;
+                    }
+                }
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+                System.out.println("DELIVERED!");
+            }
+        });
+        mqttService.connect();
+        return mqttService;
+    }
+
+    private void setupChart(LineChart chart) {
+        chart.setDrawGridBackground(true);
+        chart.setTouchEnabled(true);
+        chart.setDragEnabled(true);
+        chart.setScaleEnabled(true);
+        chart.setPinchZoom(false);
+        chart.setBackgroundColor(Color.BLACK);
+    }
+
+    private void createNotificationsChannel() {
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "ThirdChallenge", importance);
+        channel.setDescription("Third Challenge Notifications");
+        NotificationManager notificationManager = requireActivity().getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(channel);
+    }
 
 }
